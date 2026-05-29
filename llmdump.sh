9096873c7365e5878c80860f8e8aea5f756e387d
Henry the Helpful Squire <henrythehelpfulsquire@gmail.com>
2026-05-29 17:32:10 -0500

    add 'stop' and 'start' as alias commands

--- llmdump.sh
+++ llmdump.sh
@@ -16,22 +16,22 @@ export LLMDUMP_FLAG_FILE="$HOME/.local/share/llmdump/capture.flag"
 # 🎯 Main Multiplexer Function
 llmdump() {
     case "$1" in
-        on|enable)
+        on|enable|start)
             _llmdump_system_on
             _llmdump_session_on
             _llmdump_status "full"
             ;;
-        off|disable)
+        off|disable|stop)
             _llmdump_system_off
             _llmdump_session_off
             _llmdump_status "compact"
             ;;
         session)
-            if [ "$2" = "on"  ] || [ "$2" = "enable" ]; then
+            if [ "$2" = "on"  ] || [ "$2" = "enable" ] || [ "$2" = "start" ]; then
                 _llmdump_system_on
                 _llmdump_session_on
                 _llmdump_status "full"
-            elif [ "$2" = "off" ] || [ "$2" = "disable" ]; then
+            elif [ "$2" = "off" ] || [ "$2" = "disable" ] || [ "$2" = "stop" ]; then
                 _llmdump_session_off
                 _llmdump_status "full"
             else
@@ -39,10 +39,10 @@ llmdump() {
             fi
             ;;
         system)
-            if [ "$2" = "on"  ] || [ "$2" = "enable" ]; then
+            if [ "$2" = "on"  ] || [ "$2" = "enable" ] || [ "$2" = "start" ]; then
                 _llmdump_system_on
                 _llmdump_status "full"
-            elif [ "$2" = "off" ] || [ "$2" = "disable" ]; then
+            elif [ "$2" = "off" ] || [ "$2" = "disable" ] || [ "$2" = "stop" ]; then
                 _llmdump_system_off
                 _llmdump_session_off
                 _llmdump_status "compact"
